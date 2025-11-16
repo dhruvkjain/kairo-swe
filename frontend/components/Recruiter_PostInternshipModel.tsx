aea138253e02bf871f0c77d4730846642336796f
Jevik Rakholiya <202301276@dau.ac.in>
2025-11-16 12:08:48 +0000

    feat(api): add applyInternship route and update frontend to use it

--- frontend/components/Recruiter_PostInternshipModel.tsx
+++ frontend/components/Recruiter_PostInternshipModel.tsx
@@ -99,7 +99,7 @@ const Recruiter_PostInternshipModal = ({ id, onClose }: RecruiterPostInternshipM
         handleClose();
       }
     } catch (error) {
-      console.error('❌ Network error:', error);
+      console.error(' Network error:', error);
       alert('Something went wrong while publishing internship.');
     } finally {
       setIsSubmitting(false);
